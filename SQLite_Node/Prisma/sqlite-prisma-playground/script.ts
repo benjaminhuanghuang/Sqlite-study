8720d210c3f7141576b9360154e35b12bac39246
benjanmin huang <benjaminhuanghuang@gmail.com>
2026-09-24 23:31:09 -0700

    update

--- SQLite_Node/Prisma/sqlite-prisma-playground/script.ts
+++ SQLite_Node/Prisma/sqlite-prisma-playground/script.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from './generated/prisma/client'
 
-const prisma = new PrismaClient()
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter })
 
 // A `main` function so that you can use async/await
 async function main() {
